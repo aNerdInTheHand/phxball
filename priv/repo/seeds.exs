7f3cc7b4256a69bf698d25e8afbaa022b1067bad
Nick Holvast <nick.holvast@hotmail.co.uk>
2023-08-29 13:34:47 +0100

    chop deed code from seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,8 +1,4 @@
 defmodule Phxball.Seeds do
-
-  alias Faker.Date
-  alias Faker.Person, as: FakePerson
-
   alias Phxball.Repo
   alias Phxball.Clubs.Club
   alias Phxball.People.Person
@@ -17,57 +13,6 @@ defmodule Phxball.Seeds do
   end
 
   def insert_all(), do: Teams.populate()
-
-  defp insert_person(club_id) do
-    earliest_dob = ~D[1982-01-01]
-    latest_dob = ~D[2007-01-01]
-    min_wage = 1000
-    max_wage = 150000
-
-    Repo.insert!(%Person{
-      dob: Date.between(earliest_dob, latest_dob),
-      first_name: String.first(FakePerson.first_name()) <> ".",
-      last_name: FakePerson.last_name(),
-      gender: :male,
-      role: :player,
-      wage: Faker.random_between(min_wage, max_wage),
-      reputation: Faker.random_between(0, 100),
-      club_id: club_id
-      },
-    returning: [:id])
-  end
-
-  defp insert_player(person_id, position) do
-    positions = [:goalkeeper, :defender, :midfielder, :attacker]
-    Repo.insert! %Player{
-      att_heading: Faker.random_between(1, 100),
-      att_link_up: Faker.random_between(1, 100),
-      att_shooting: Faker.random_between(1, 100),
-      def_attacking: Faker.random_between(1, 100),
-      def_heading: Faker.random_between(1, 100),
-      def_marking: Faker.random_between(1, 100),
-      def_positioning: Faker.random_between(1, 100),
-      def_tackling: Faker.random_between(1, 100),
-      gk_crosses: Faker.random_between(1, 100),
-      gk_distribution: Faker.random_between(1, 100),
-      gk_shotstopping: Faker.random_between(1, 100),
-      men_aggression: Faker.random_between(1, 100),
-      men_composure: Faker.random_between(1, 100),
-      men_fortitude: Faker.random_between(1, 100),
-      mid_dribbling: Faker.random_between(1, 100),
-      mid_flair: Faker.random_between(1, 100),
-      mid_passing: Faker.random_between(1, 100),
-      mid_positioning: Faker.random_between(1, 100),
-      mid_shooting: Faker.random_between(1, 100),
-      phys_cardio: Faker.random_between(1, 100),
-      phys_recovery: Faker.random_between(1, 100),
-      phys_resilience: Faker.random_between(1, 100),
-      phys_speed: Faker.random_between(1, 100),
-      position: position,
-      person_id: person_id
-    }
-  end
-
 end
 
 Phxball.Seeds.delete_all()
