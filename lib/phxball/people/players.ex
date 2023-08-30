78bd74eb0d24626cd2bd401745eaff57c7147e0f
Nick Holvast <nick.holvast@hotmail.co.uk>
2023-08-30 11:47:54 +0100

    some overall calculation stuff

--- lib/phxball/people/players.ex
+++ lib/phxball/people/players.ex
@@ -1,5 +1,21 @@
 defmodule Phxball.Players.Players do
-  def calcuate_overall(player) do
-    85
+  alias Phxball.People.Const
+
+  def calculate_overall(%{position: position} = player) do
+    cond do
+      to_string(position) =~ "gk" -> calculate_gk_overall(player)
+      to_string(position) =~ "def_lb" -> calculate_lb_rb_overall(player)
+      to_string(position) =~ "def_rb" -> calculate_lb_rb_overall(player)
+      to_string(position) =~ "def_cb" -> calculate_cb_overall(player)
+      to_string(position) =~ "def_cm" -> calculate_cm_overall(player)
+      to_string(position) =~ "mid_l" -> calculate_lw_rw_overall(player)
+      to_string(position) =~ "mid_r" -> calculate_lw_rw_overall(player)
+      to_string(position) =~ "att_" -> calculate_att_overall(player)
+      true -> 50
+    end
+  end
+
+  defp calculate_gk_overall(player) do
+
   end
 end
